9e8c895096b19765d389d739004510060455060a
Bryan Powell <bryanpowell@utexas.edu>
2017-02-05 23:17:27 -0500

    anim

--- js/site.js
+++ js/site.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   var index = 0;
   var buttons = $('.project-button')
   var images = $('.project-presentation > img')
-  var timer = setInterval(function(){ setProject(); }, 5000)
+  var timer = setInterval(function(){ setProject(); $('#heroButton').animateCss('tada'); }, 5000)
 
   var descriptions = [
     'A Raspberry Pi based touch screen radio. User can log into their Pandora account to listen to their radio stations and rate songs.',
@@ -14,14 +14,9 @@ $(document).ready(function(){
     'Like golf, except instead of aiming for a hole you must aim for all of the objects on each course.'
   ]
 
-  $('.project-button').click(function(){
-    $('.selected').removeClass('selected', 400, 'swing');
-    $(this).addClass('selected', 400, 'swing');
-    clearInterval(timer);
-    index = jQuery.inArray(this, buttons)
-    nextImage()
-    timer = setInterval(function(){ setProject(); }, 5000)
-  })
+
+
+
 
   $("a").on('click', function(event) {
     if (this.hash !== "") {
@@ -35,6 +30,27 @@ $(document).ready(function(){
     }
   });
 
+  $.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+  });
+
+  $('.project-button').click(function(){
+    $('.selected').removeClass('selected', 400, 'swing');
+    $(this).addClass('selected', 400, 'swing');
+    clearInterval(timer);
+    index = jQuery.inArray(this, buttons)
+    nextImage()
+    timer = setInterval(function(){
+      setProject();
+      $('#heroButton').animateCss('tada')
+    }, 5000)
+  })
+
   function setProject() {
     $(buttons[index]).removeClass('selected', 400, 'swing');
     index = index < 3 ? index + 1 : 0
@@ -43,20 +59,10 @@ $(document).ready(function(){
     nextImage()
   }
 
-  // has new index already
   function nextImage() {
     $('.overlay > p').text(descriptions[index])
     var curr = $(images[index])
     $('.active').removeClass('active')
     curr.addClass("active")
-    // prev.fadeOut(250, function(){
-    //
-    // })
-    // $('.project-presentation > img').fadeOut(200, function() {
-    //   $('.project-presentation > img').attr('src', "media/projects/" + index + ".png")
-    // })
-    //
-    // $('.project-presentation > img').fadeIn(200)
   }
-
 })
